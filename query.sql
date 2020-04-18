f8121b76dd0e7d639ffaa9108bfb1be051739ad1
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-04-18 21:40:49 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -5,10 +5,25 @@ count(bartenderbar.bartender_name) as total_bartenders from bartenderbar
 group by bartenderbar.bar_name
 order by total_bartenders desc
 
---Процент барменов в каждом баре
+--Соотношение барменов в каждом баре к общему их кол-ву
+
 select bartenderbar.bar_name,
 concat(round(count(bartenderbar.bartender_name)*100/(
 select count(*) from bartenderbar)),'%') as procent_of_total_bartenders from bartenderbar
 group by bartenderbar.bar_name
 
---Зависимость кол-ва коктейлей от локации
+--зависимость кол-ва коктейлей от расположения бара
+             
+select count(cocktailbartender.cocktail_name) as quantity_of_cocktails,
+bar.location
+from cocktailbartender
+             
+--join bartender_name
+             
+left join bartenderbar on bartenderbar.bartender_name=cocktailbartender.bartender_name
+
+--join bar_name
+             
+left join bar on bar.bar_name=bartenderbar.bar_name
+group by bar.location
+order by quantity_of_cocktails desc
