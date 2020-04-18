a14d908020b1f9d53db5d2c6c03d393cd64de07e
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-04-18 21:03:09 +0300

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,13 @@
+--Кол-во барменов в каждом баре
+
+select bartenderbar.bar_name,
+count(bartenderbar.bartender_name) as total_bartenders from bartenderbar
+group by bartenderbar.bar_name
+order by total_bartenders desc
+
+--Процент барменов в каждом баре
+select bartenderbar.bar_name,
+concat(round(count(bartenderbar.bartender_name)*100/(
+select count(*) from bartenderbar)),'%') as procent_of_total_bartenders from bartenderbar
+group by bartenderbar.bar_name
+
