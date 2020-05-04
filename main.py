88c947fa7f8cf407d85661d7c11e4ee0b512de89
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-04 17:29:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ connection = cx_Oracle.connect(username, password, database)
 cursor = connection.cursor()
 
 query1 = """
-select bartenderbar.bar_name,
-count(bartenderbar.bartender_name) as total_bartenders from bartenderbar
-group by bartenderbar.bar_name
+select humanbar.bar_name,
+count(humanbar.human_name) as total_bartenders from humanbar
+group by humanbar.bar_name
 order by total_bartenders desc
 """
 cursor.execute(query1)
@@ -21,10 +21,10 @@ for row in cursor:
     print(row)
 
 query2 = """
-select bartenderbar.bar_name,
-concat(round(count(bartenderbar.bartender_name)*100/(
-select count(*) from bartenderbar)),'%') as procent_of_total_bartenders from bartenderbar
-group by bartenderbar.bar_name
+select humanbar.bar_name,
+concat(round(count(humanbar.human_name)*100/(
+select count(*) from humanbar)),'%') as procent_of_total_bartenders from humanbar
+group by humanbar.human_name
 """
 cursor.execute(query2)
 print("\n Bar_name - %_quantity_of_bartenders")
@@ -33,12 +33,12 @@ for row in cursor:
     print(row)
 
 query3 = """
-select count(cocktailbartender.cocktail_name) as quantity_of_cocktails,
-bar.location
-from cocktailbartender
-    left join bartenderbar on bartenderbar.bartender_name=cocktailbartender.bartender_name             
-left join bar on bar.bar_name=bartenderbar.bar_name
-group by bar.location
+select count(chumancocktail.cocktail_name) as quantity_of_cocktails,
+bar.city_name
+from humancocktail
+    left join humanbar on humanbar.human_name=humancocktail.human_name             
+left join bar on bar.bar_name=humanbar.bar_name
+group by bar.city_name
 order by quantity_of_cocktails desc
 """
 cursor.execute(query3)
