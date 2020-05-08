3ba08cd7f3f971ae326d223166079786ccbe3a26
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-08 19:22:36 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -3,14 +3,14 @@
 select humanbar.bar_name,
 count(humanbar.human_name) as total_humans from humanbar
 group by humanbar.bar_name
-order by total_humans desc
+order by total_humans desc;
 
 --Соотношение барменов в каждом баре к общему их кол-ву
 select humanbar.bar_name,
 concat(round(count(humanbar.human_name)*100/(
 select count(*) from humanbar)),'%') as procent_of_total_bartenders from humanbar
 group by humanbar.bar_name
-order by procent_of_total_bartenders desc
+order by procent_of_total_bartenders desc;
 
 --зависимость кол-ва коктейлей от расположения бара
 
@@ -22,4 +22,4 @@ left join humanbar on humanbar.human_name=humancocktail.human_name
 --join bar_name         
 left join bar on bar.bar_name=humanbar.bar_name
 group by bar.city_name
-order by quantity_of_cocktails desc
+order by quantity_of_cocktails desc;
